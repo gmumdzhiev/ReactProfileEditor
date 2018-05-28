f2ad12a3ca287c9fd1fc9847e95e509024e68a3a
Georgi Mumdzhiev <george.mumdzhiev@gmail.com>
2018-05-28 12:22:57 +0200

    making components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
 import React, { Component } from 'react';
-
+import {connect} from 'react-redux';
+import StateForm from './StateForm.js';
+import {newProfile} from './store/account.js'
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-
-
-      </div>
-    );
+  componentWillMount(){
+    this.props.dispatch(newProfile())
   }
+
+
+render(){
+  const{
+    firstName,
+    lastName,
+    age,
+    sex,
+    employed,
+    bio,
+  } = this.props
+
+  if (true){
+    return <div> You have succesfully registrated </div>
+}
+return (
+  <div>
+    <StateForm/>
+    </div>
+);
+}
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+  firstName: state.name,
+  lastName: state.name,
+  age:state.age,
+
+})
+
+export default connect(mapStateToProps)(App);
