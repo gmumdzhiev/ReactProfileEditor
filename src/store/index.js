630fda1bd5a2e3286fcafd8398f2b29b5cfc3f95
Georgi Mumdzhiev <george.mumdzhiev@gmail.com>
2018-05-29 10:46:11 +0200

    addin stuff that i need

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,22 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import { reducer as formReducer } from 'redux-form'
+
+import { reducer as userReducer, userMiddleware } from './user'
+
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
+
 const rootReducer = combineReducers({
   form: formReducer,
+  user: userReducer,
 })
 
-const middleware = applyMiddleware(thunk)
+const middleware = applyMiddleware(thunk, userMiddleware)
+
 
 const store = createStore(rootReducer, composeWithDevTools(middleware))
+
+
 
 export default store
